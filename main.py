27605dc169493a23ea91d6a5052a292f3f3410ae
aquib-sh <shaikhaquib394@gmail.com>
2020-08-08 17:41:47 +0530

    Add export functions to menuitems 1. export_html 2. export_json 3. export_txt 4. export_pdf

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ from reader import MetaReader
 from writer import MetaWriter
 from PIL import ImageTk,Image
 
-def test():
-    pass
 
 class Main():
 
@@ -26,9 +24,10 @@ class Main():
         self.spaces = "                                 \n\n "
         self.txt = ""
 
-        self.DEFAULT_TEXT_POSITION = (350,460)
-        self.INTERMEDIATE_TEXT_POSITION = (350,550)
-        self.EXPANDED_TEXT_POSITION = (350, 880)
+        self.DEFAULT_TEXT_POSITION = (350,460)      # If data length < 1000
+        self.INTERMEDIATE_TEXT_POSITION = (350,550) # If data length > 1000 
+        self.EXPANDED_TEXT_POSITION = (350, 880)    # If data length < 2000
+        self.NON_IMAGE_FILE_POSITION = (350,200)    # If data is from a non-image file
 
         # Getting the root of Application class for flexibility
         self.root = self.obj.return_root()
@@ -38,7 +37,7 @@ class Main():
         self.canvas = Canvas(self.frame)
 
         # Gets the path of the file currently opened
-        self.current_filename = ""
+        self.current_filename = "  "
 
         # Gets some variable for function transfers
         self.elements_dict = {}
@@ -62,7 +61,7 @@ class Main():
         bar = self.obj.add_menu("File",elements)
 
         # Add menu options in the form of dictionary
-        menu = self.obj.add_menu("Export",{"HTML":test,"JSON":test,"PDF":test,"Text":test},handle=bar)
+        menu = self.obj.add_menu("Export",{"HTML":self.export_html,"JSON":self.export_json,"PDF":self.export_pdf,"Text":self.export_txt},handle=bar)
         
 
     # Opens up the file choosing dailogbox    
@@ -72,11 +71,18 @@ class Main():
             2> Displays preview if the file is an image
             3> Displays metadata
         """
-
         self.canvas.pack(fill=Y,expand=True)
+        # Saving the previous filename
+        temp = self.current_filename
+
         # Opens up a file choose dialog box
         self.current_filename = filedialog.askopenfilename(initialdir="C:\\Users\\.*\\Pictures",title="Select a file",filetypes=(("all files",".*.*"),))
         
+        # If current filename is Null i.e if the user exits the filechooser window without selecting anything
+        # then revert the path in the entry field back to what it was.
+        if self.current_filename == "":
+            self.current_filename = temp
+
         # Delete whatever is in there already
         self.entry_.delete(0,END)
 
@@ -84,13 +90,26 @@ class Main():
         self.entry_.insert(0,str(self.current_filename))
 
         # If the file is an image we will display its's preview
+        global non_image_file
         is_image = self.is_image(self.current_filename)
-        if is_image != None:
-            self.display_image(self.current_filename)
-
+        if is_image == True:
+            non_image_file = False
+            self.display_image(self.current_filename) 
+        else:
+            non_image_file = True
         # Display the meta-data content
         self.display_data(self.current_filename)
 
+
+    def open_save_as(self,type_="Text",format=".txt"):
+        """ Opens saveas file chooser for export options """
+        save_file = filedialog.asksaveasfilename(initialdir="C:\\Users\\.*\\Pictures",title="Select a file",filetypes=((type_,format),))
+        base_file = save_file.split("/")
+        if format in base_file[len(base_file)-1]:
+            return save_file
+        else:
+            save_file += format
+        return save_file
 
     def display_image(self,file_path):
         """ If the file is an image then display its's preview. """
@@ -106,18 +125,21 @@ class Main():
         text_ = self.get_metadata(file_path)
         self.canvas.itemconfigure(self.text_obj, text=text_, state="normal")
         length = len(text_)
-        if length > 2000:
-            self.canvas.coords(self.text_obj,self.EXPANDED_TEXT_POSITION)
-        elif length > 1000:
-            self.canvas.coords(self.text_obj,self.INTERMEDIATE_TEXT_POSITION)
+
+        if(non_image_file):
+            self.canvas.coords(self.text_obj,self.NON_IMAGE_FILE_POSITION)
+            self.canvas.itemconfigure(self.img_obj,state="hidden")
         else:
-            self.canvas.coords(self.text_obj,self.DEFAULT_TEXT_POSITION)
+            if length > 2000:
+                self.canvas.coords(self.text_obj,self.EXPANDED_TEXT_POSITION)
+            elif length > 1000:
+                self.canvas.coords(self.text_obj,self.INTERMEDIATE_TEXT_POSITION)
+            else:
+                self.canvas.coords(self.text_obj,self.DEFAULT_TEXT_POSITION)
 
 
     # Check if the file is an image
     def is_image(self,path):
-        if len(path) == 0:
-            return None
         p = path.split("/")
         name = p[len(p) -1].split(".")[1]
         if name in self.image_extensions:
@@ -126,6 +148,7 @@ class Main():
 
 
     def browse(self):
+
         browse_button = self.obj.add_button("Browse",0,1,is_entry_button=True)
         browse_button.config(command=self.open_file_chooser)
         
@@ -174,15 +197,44 @@ class Main():
 
 
     def get_metadata(self,file):
+        """ Gets metadata from the file and return it in String form. """
         global reader_
         reader_ = MetaReader(file)
         data = reader_.get_dict()
         text = ""
 
         for key,value in data.items():
-            text += "{} : {}\n".format(key,value)
-        
+            text += "{} : {}\n".format(key,value)        
         return text
+
+
+    def export_html(self):
+        """ Exports the metadata as an HTML file. """
+        filename = self.open_save_as(type_="HTML",format=".html")
+        data = reader_.get_html()
+        writer_ = MetaWriter(data)
+        writer_.write_to_html(filename)
+
+    def export_txt(self):
+        """ Exports the metadata as a Text file. """
+        filename = self.open_save_as(type_="Text",format=".txt")
+        data = reader_.get_dict()
+        writer_ = MetaWriter(data)
+        writer_.write_to_txt(filename)
+    
+    def export_json(self):
+        """ Exports the metadata as a JSON file. """
+        filename = self.open_save_as(type_="JSON",format=".json")
+        data = reader_.get_dict()
+        writer_ = MetaWriter(data)
+        writer_.write_to_json(filename)
+
+    def export_pdf(self):
+        """ Exports the metadata as a PDF file. """
+        filename = self.open_save_as(type_="PDF",format=".pdf")
+        data = reader_.get_dict()
+        writer_ = MetaWriter(data)
+        writer_.write_to_pdf(filename)
 
     # Create canvas and all the images and text to it
     def end(self):
